c172f7e6a548438e39b92c53af9bbef196460fb8
Jiyun Kim <wayandway@kakao.com>
2024-12-28 22:52:38 +0900

    refactor: 스타일, 레이아웃 폴더 구조 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,11 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import '../src/app/styles/globals.css';
+import RootLayout from '@/app/layouts';
 
-const geistSans = localFont({
-  src: '../public/fonts/GeistMonoVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: '../public/fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
-
-export const metadata: Metadata = {
-  title: '머니로그',
-  description: '경제 블로그 플랫폼',
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body>
+        <RootLayout>{children}</RootLayout>
+      </body>
     </html>
   );
 }
