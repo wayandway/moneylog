2048d4d463944661379917a59da7bb4ef4ced76e
Jiyun Kim <wayandway@kakao.com>
2024-12-29 00:02:44 +0900

    refactor: 레이아웃 파일 구조 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,33 @@
-import RootLayout from '@/app/layouts';
+import type { Metadata } from 'next';
+import '../src/shared/styles/reset.css';
+import '../src/shared/styles/globals.css';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: '머니로그',
+  description: '경제 블로그 플랫폼',
+  openGraph: {
+    title: '머니로그',
+    description: '경제 블로그 플랫폼',
+    type: 'website',
+    // url: '',
+    // images: [
+    //   {
+    //     url: '',
+    //     width: 800,
+    //     height: 600,
+    //   },
+    // ],
+  },
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="ko">
       <body>
-        <RootLayout>{children}</RootLayout>
+        <main>{children}</main>
       </body>
     </html>
   );
